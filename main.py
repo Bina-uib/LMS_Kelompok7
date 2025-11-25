af4b2b1c59c37bfb0ebbd78af150089b0377d558
randikhsan <randifahrul54@gmail.com>
2025-11-25 19:04:56 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from tkinter import*
+from tkinter import ttk
+
+class mainapp:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("LMS")
+        self.root.geometry("500x200")
+        ttk.Label(self.root,text="usename: ").pack()
+        self.entry_username=ttk.Entry(self.root)
+        self.entry_username.pack()
+        ttk.Label(self.root,text="pasword:").pack()
+        self.entry_pasword=ttk.Entry(self.root,show="*")
+        self.entry_pasword.pack()
+        ttk.Button(self.root,text="login",command=self.proses_login ).pack(pady='20')
+
+    def proses_login (self):
+        username=self.entry_username.get()
+        pasword=self.entry_pasword.get()
+
+
+root=Tk() 
+app=mainapp(root)
+root.mainloop()
