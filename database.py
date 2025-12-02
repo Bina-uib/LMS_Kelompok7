d120ff38bdb2282961cc587e3586454705b153fe
hazimulf97-cmyk <hazimulf97@gmail.com>
2025-12-02 20:57:40 +0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,14 @@
+import mysql.connector as mysql_conn
+
+class Database:
+	def __init__(self) :
+		self.mydb = mysql_conn.connect(
+	 		host="localhost",
+	 		user="root",
+	 		password="",
+	 		database="data_mahasiswa"
+		)
+
+		self.cursor = self.mydb.cursor()
+
+db_conn = Database()
