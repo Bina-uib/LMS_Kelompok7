369a04a65f12a11b412b8aea3a9f4729bfda4c12
randikhsan <randifahrul54@gmail.com>
2025-12-08 14:11:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class mainapp:
     def __init__(self, root):
         self.root = root
         self.root.title("LMS")
-        self.root.geometry("500x300")
+        self.root.geometry("5000x3000")
         self.root.configure(bg="#e8e8e8")   
 
         self.halaman_login()
@@ -90,10 +90,65 @@ class mainapp:
         messagebox.showinfo("Info", "Menu Data Mahasiswa dibuka.\nSambungkan ke mahasiswa.py")
 
     # ============================
-    # HALAMAN MATA KULIAH
+    # HALAMAN PILIH KELAS MATA KULIAH
     # ============================
     def buka_mata_kuliah(self):
-        messagebox.showinfo("Info", "Menu Mata Kuliah dibuka.\nSambungkan ke prodi.py")
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+        self.root.configure(bg="#f2f2f2")
+
+        Button(self.root, text="Kembali", bg="#0275d8", fg="white",
+               command=self.halaman_menu).place(x=10, y=10)
+
+        Label(self.root, text="Pilih Kelas", bg="#f2f2f2",
+              font=("Segoe UI", 17, "bold")).pack(pady=25)
+
+        frame = Frame(self.root, bg="#f2f2f2")
+        frame.pack(pady=10)
+
+        Button(frame, text="KELAS TIMA", width=20, height=2,
+               command=lambda: self.tampilkan_mk("tima")).grid(row=0, column=0, padx=10, pady=10)
+
+        Button(frame, text="KELAS TIMB", width=20, height=2,
+               command=lambda: self.tampilkan_mk("timb")).grid(row=0, column=1, padx=10, pady=10)
+
+    # ============================
+    # TAMPILKAN DATA MATA KULIAH BERDASARKAN KELAS
+    # ============================
+    def tampilkan_mk(self, nama_kelas):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+        Button(self.root, text="Kembali", bg="#0275d8", fg="white",
+               command=self.buka_mata_kuliah).place(x=10, y=10)
+
+        Label(self.root, text=f"DAFTAR MATA KULIAH - {nama_kelas.upper()}",
+              font=("Segoe UI", 16, "bold")).pack(pady=20)
+
+        frame = Frame(self.root)
+        frame.pack()
+
+        kolom = ("id_mk", "nama_mk", "dosen")
+
+        tabel = ttk.Treeview(frame, columns=kolom, show="headings", height=10)
+        tabel.pack()
+
+        tabel.heading("id_mk", text="ID")
+        tabel.heading("nama_mk", text="Nama Mata Kuliah")
+        tabel.heading("dosen", text="Dosen Pengampu")
+
+        tabel.column("id_mk", width=50)
+        tabel.column("nama_mk", width=200)
+        tabel.column("dosen", width=150)
+
+        # AMBIL DATA DARI DATABASE
+        query = f"SELECT * FROM {nama_kelas}"
+        database.db_conn.cursor.execute(query)
+        data = database.db_conn.cursor.fetchall()
+
+        for row in data:
+            tabel.insert("", "end", values=row)
 
 
 root = Tk()
